08c8ccd7117f121ec2a3c3df890674c3b45413dd
Zk77 <60575976+nbgsmk@users.noreply.github.com>
2023-09-06 00:43:55 +0200

    Initial

--- app/src/main/java/cc/kostic/egzekjutor/ui/home/HomeFragment.java
+++ app/src/main/java/cc/kostic/egzekjutor/ui/home/HomeFragment.java
@@ -15,10 +15,8 @@ public class HomeFragment extends Fragment {
 
 	private FragmentHomeBinding binding;
 
-	public View onCreateView(@NonNull LayoutInflater inflater,
-							 ViewGroup container, Bundle savedInstanceState) {
-		HomeViewModel homeViewModel =
-				new ViewModelProvider(this).get(HomeViewModel.class);
+	public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		HomeViewModel homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
 
 		binding = FragmentHomeBinding.inflate(inflater, container, false);
 		View root = binding.getRoot();
